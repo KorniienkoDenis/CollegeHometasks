77aa0e4d85bf0fae9bb6a673f15f90e5c043abfe
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-04 15:44:13 +0300

    Condition of the problem (Task 1)

---
+++ CollegeHometask3_Task_1/CollegeHometask3_Task_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+/*
+
+	Завдання 1. UKRAINIAN VERSION COMMENT.
+	===============================================================
+	Напишіть програму для розрахунку по вказаних формулах. Провести
+	модульне та інтеграційне тестування створеного алгоритму.
+
+	Варіант 10.
+
+	z1 = (sin(pi/2 + 3alpha)) / 1 - sin(3alpha - pi);
+	z2 = ctg((5/4pi) + (3/2alpha));
+	===============================================================
+
+	---------------------------------------------------------------
+
+	Task 1. ENGLISH VERSION COMMENT.
+	====================================================================
+	Write a program for calculation according to the indicated formulas.
+	Conduct module and integration testing of the created algorithm.
+
+	Variant 10.
+
+	z1 = (sin(pi/2 + 3alpha)) / 1 - sin(3alpha - pi);
+	z2 = ctg((5/4pi) + (3/2alpha));
+	====================================================================
+
+*/
+
+int main()
+{
+
+}

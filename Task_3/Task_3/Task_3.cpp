c14d72cf7db92e183ac8920d7e730844338589e1
Denis Korniienko <kubikrubikmegaminks@gmail.com>
2023-03-10 03:48:12 +0300

    Task #3 v1

--- Task_3/Task_3/Task_3.cpp
+++ Task_3/Task_3/Task_3.cpp
@@ -1,20 +1,110 @@
-// Task_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <ctime>
+#include <fstream>
+#include <windows.h>
+
+/*
+
+	Task 3.
+	1) Specify a two-dimensional array 13x13.
+	2) Write a program that will display all elements
+	   of the array in the form of a matrix with equal
+	   columns on the screen and save them to a file.
+	3) Write a program that reads the saved array from the file,
+	   displays all its elements on the screen, and performs
+	   the task of the variant.
+	4) Conduct module and integration testing of the created algorithm.
+
+	Variant 10.
+	1) A matrix element is called a local minimum if it is strictly smaller
+	than all of it's neighbors. Calculate the number of local minimals
+	of a given matrix of size 13x13.
+
+	2) Find the sum of modules of the elements which located above the main diagonal.
+
+*/
+
+HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand(time(nullptr));
+
+	const int row = 13;
+	const int column = 13;
+
+	int matrix[row][column];
+
+
+
+	for (int indexRow = 0; indexRow < row; indexRow++)
+		for (int indexColumn = 0; indexColumn < column; indexColumn++)
+			matrix[indexRow][indexColumn] = -20 + rand() % 50;
+
+
+
+	std::cout << "Matrix 13x13 : ";
+	std::cout << std::endl;
+
+	SetConsoleTextAttribute(handle, 3);
+	for (int indexRow = 0; indexRow < row; indexRow++)
+	{
+		for (int indexColumn = 0; indexColumn < column; indexColumn++)
+		{
+			std::cout << matrix[indexRow][indexColumn] << " \t";
+		}
+
+		std::cout << std::endl;
+	}
+	SetConsoleTextAttribute(handle, 7);
+
+
+
+	std::ofstream fout("D:\\matrix.txt");
+
+	if (!fout)
+		std::cout << "Cannot opent this file!\n";
+
+	for(int indexRow = 0; indexRow < row; indexRow++)
+	{
+		for (int indexColumn = 0; indexColumn < column; indexColumn++)
+		{
+			fout << matrix[indexRow][indexColumn] << " \t";
+		}
+
+		fout << " " << std::endl;
+	}
+
+	fout.close();
+
+
+
+	std::ifstream fin("D:\\matrix.txt");
+
+	if (!fout)
+		std::cout << "Cannot open thsi file!\n";
+
+	std::cout << std::endl;
+	std::cout << "The matrix from the file matrix.txt: ";
+	std::cout << std::endl;
+
+	SetConsoleTextAttribute(handle, 3);
+	for (int indexRow = 0; indexRow < row; indexRow++)
+	{
+		for (int indexColumn = 0; indexColumn < column; indexColumn++)
+		{
+			fin >> matrix[indexRow][indexColumn];
+
+			std::cout << matrix[indexRow][indexColumn] << " \t";
+		}
+
+		std::cout << std::endl;
+	}
+	SetConsoleTextAttribute(handle, 7);
+
+
+
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
